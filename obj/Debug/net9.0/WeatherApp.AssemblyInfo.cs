d75119de6fc438f37885726682eea8a0cd78fb9b
Siddeeq <saeedsinanan23@gmail.com>
2025-12-10 11:27:24 -0400

    ui

--- obj/Debug/net9.0/WeatherApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/WeatherApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d2fc17adbcd7eeb66d7a13405bfea2dff5faed0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce77a1502a5b938fe42c958e95d6553539389477")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
